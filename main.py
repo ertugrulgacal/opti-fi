b3c248087b2c54e48977554d7c2a25976f9b6388
Ertugrul <ergacal16@gmail.com>
2024-03-05 01:30:29 +0300

    Error handling

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import open3d as o3d
 import numpy as np
 
 main_folder_path = "/Users/egkubo/Stanford3DISD/Stanford3dDataset_v1.2_Aligned_Version"
-areas_to_read = ["Area_1", "Area_2"]
+areas_to_read = ["Area_1"]
+rooms_to_read = ["office_1", "office_2", "office_3", "office_4", "office_5", "office_6"]
 
 def read_room(room_path):
     combined_data = np.empty((0, 6))  # (X, Y, Z, R, G, B)
 
     for filename in os.listdir(room_path):
-        if filename.endswith(".txt"):
+        if filename.endswith(".txt") and filename[:-4] in rooms_to_read:
             txt_file_path = os.path.join(room_path, filename)
-            
-            data = np.loadtxt(txt_file_path)  # Read txt
-            combined_data = np.concatenate((combined_data, data), axis=0)  # Combine txt files
+
+            try:
+                data = np.loadtxt(txt_file_path)  # Read txt
+                combined_data = np.concatenate((combined_data, data), axis=0)  # Combine txt files
+            except ValueError as e:
+                print(f"Error reading {txt_file_path}: {e}. Skipping.")
 
     return combined_data
 
@@ -22,12 +26,12 @@ def read_areas(main_folder, area_names):
 
     for area_name in area_names:
         area_path = os.path.join(main_folder, area_name)
-        
+
         if os.path.isdir(area_path):  # Check if area folder is a directory
             for room_folder in os.listdir(area_path):
                 room_path = os.path.join(area_path, room_folder)
 
-                if os.path.isdir(room_path):  # Check if room folder is a directory
+                if os.path.isdir(room_path) and room_folder in rooms_to_read:  # Check if room folder is a directory and in the specified list
                     combined_data = read_room(room_path)
                     all_data = np.concatenate((all_data, combined_data), axis=0)  # Combine point cloud data returned from the read_room function
 

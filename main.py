31a3177ef6e0a0f8fa82d44da0dc0552f1aa0722
Ertugrul <ergacal16@gmail.com>
2024-03-27 17:57:45 +0300

    Ground detection

--- main.py
+++ main.py
@@ -9,31 +9,21 @@ class Point:
         self.z = z
 
 
-def create_point_cloud_from_list(point_cloud):
-    points = [[point.x, point.y, point.z] for point in point_cloud]
+def create_point_cloud_from_file(file_path):
+    data = np.loadtxt(file_path)
+
+    # Get first 3 indices which are (X, Y, Z)
+    points = data[:, :3]
+
+    # Get last 3 indices which are (R, G, B)
+    colors = data[:, 3:]
+
+    # Define a point cloud object and fill it with data we have read
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
+    pcd.colors = o3d.utility.Vector3dVector(colors / 255.0)  # Normalize color values to [0, 1]
+
     return pcd
-
-
-def read_point_cloud_data(file_path):
-    point_cloud = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            components = line.split()
-            x = float(components[0])
-            y = float(components[1])
-            z = float(components[2])
-            point_cloud.append(Point(x, y, z))
-    return point_cloud
-
-
-def downsampled_pcd(pcd):
-    return pcd.voxel_down_sample(voxel_size=0.05)
-
-
-def vertex_normal_estimation(pcd):
-    return pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 
 
 def see_point(geometries, point):
@@ -46,17 +36,29 @@ def see_point(geometries, point):
     o3d.visualization.draw_geometries(geometries)
 
 
+def ground_detection(pcd, threshold=0.2):
+    pcd_colors = np.asarray(pcd.colors)
+    z_min = min(pcd.points, key=lambda x: x[2])[2]
+
+    for i in range(len(pcd.points)):
+        if pcd.points[i][2] <= z_min + threshold:
+            pcd_colors[i] = [0, 1, 0]
+
+    pcd.colors = o3d.utility.Vector3dVector(pcd_colors)
+
+    return pcd
+
+
 def main():
     FILE_PATH = "teknofest_ornek_data_v1/ornek_ev_1_lidar_export.xyz"
-
-    point_cloud_data = read_point_cloud_data(FILE_PATH)
-    pcd = create_point_cloud_from_list(point_cloud_data)
+    pcd = create_point_cloud_from_file(FILE_PATH)
 
     origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5)
+
+    pcd = ground_detection(pcd)
+
     geometries = [pcd, origin]
-
     see_point(geometries, Point(1, 1, 2))
-
     #o3d.visualization.draw_geometries([pcd], point_show_normal=False)
 
 
